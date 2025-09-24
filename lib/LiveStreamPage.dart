784e485df1978190929b10299924a36ad266c005
Msyahmi <syahmiamsyar2511@gmail.com>
2025-09-24 22:21:01 +0800

    Add files via upload

---
+++ lib/LiveStreamPage.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
+
+class LiveStreamPage extends StatefulWidget {
+  const LiveStreamPage({super.key});
+
+  @override
+  State<LiveStreamPage> createState() => _LiveStreamPageState();
+}
+
+class _LiveStreamPageState extends State<LiveStreamPage> {
+  late VideoPlayerController _videoPlayerController;
+  ChewieController? _chewieController;
+  final String streamUrl = "rtsp://192.168.1.25:8554/"; // 🔴 Replace with your Pi’s IP
+
+  @override
+  void initState() {
+    super.initState();
+    _initPlayer();
+  }
+
+  Future<void> _initPlayer() async {
+    _videoPlayerController = VideoPlayerController.networkUrl(Uri.parse(streamUrl));
+
+    await _videoPlayerController.initialize();
+
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController,
+      autoPlay: true,
+      looping: true,
+      allowFullScreen: true,
+      aspectRatio: _videoPlayerController.value.aspectRatio,
+    );
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF0073B1),
+        title: const Text("Live Stream"),
+      ),
+      body: Center(
+        child: _chewieController != null &&
+            _chewieController!.videoPlayerController.value.isInitialized
+            ? Chewie(controller: _chewieController!)
+            : const CircularProgressIndicator(),
+      ),
+    );
+  }
+}
